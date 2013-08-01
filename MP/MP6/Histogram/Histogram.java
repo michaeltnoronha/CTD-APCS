238ed0a24e971911551d7a23cf43431d497df46d
michaeltnoronha <michaeltnoronha@yahoo.com>
2013-08-01 14:20:34 -0500

    MP6 initial commit

---
+++ MP/MP6/Histogram/Histogram.java
@@ -0,0 +1,12 @@
+package Histogram;
+
+public class Histogram
+{
+    private int [] values;
+    private int interval;
+    
+    public Histogram(){
+        interval = 10;
+        new values [99];
+    }
+}
